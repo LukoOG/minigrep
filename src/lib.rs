4470a4f10923d3db11ca354d11bd0494cc54dffe
LukoOG <emmanueladesipe01@gmail.com>
2026-05-01 19:40:52 +0100

    update: added a test

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results= Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push((line));
+        }
+    };
+    results
+}
+
 impl Config {
     pub fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
@@ -23,5 +33,21 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
