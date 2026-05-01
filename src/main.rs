997a55d1bed2eecd379ead4cff98b47c9681cf5a
LukoOG <emmanueladesipe01@gmail.com>
2026-05-01 19:04:11 +0100

    feat: added file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args = dbg!(args);
+
+    let query = &args[1];
+    let path = &args[2];
+    let contents = fs::read_to_string(path).expect("Should have been able to read a file");
+
+    println!("Searching for {query}");
+    println!("contents: \n:{contents}");
 }
