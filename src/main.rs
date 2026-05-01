54161fd08ce3ce1d8297a53ef8600529122f4d75
LukoOG <emmanueladesipe01@gmail.com>
2026-05-01 19:59:04 +0100

    fix: printing errors to stderror stream

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ use std::process;
 use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let args = dbg!(args);
+    // let args = dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Error {e}");
+        eprintln!("Error {e}");
         process::exit(1);
     };
 }
